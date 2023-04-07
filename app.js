795b77e37db03b59a07b06ebd0f74f3c78c81453
Juan Ercole <juan.m.ercole@gmail.com>
2023-04-07 18:56:48 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+require('colors');
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+console.clear();
+
+
+const main = async() => {
+
+  console.log('hi');
+  mostrarMenu();
+
+  pausa();
+
+}
+
+
+
+main();
+
+
+
+
+
