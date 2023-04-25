5fe185a838886743b871329aa430bba25b93acd5
Juan Ercole <juan.m.ercole@gmail.com>
2023-04-24 23:57:52 -0300

    menu interactivo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('colors');
-const { inquirerMenu } = require('./helpers/inquirer');
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
 console.clear();
 
 
@@ -10,8 +10,10 @@ const main = async() => {
   do {
 
     opt = await inquirerMenu();
-    if (opt !== '0') await pausa();
-  
+    console.log({ opt });
+
+    await pausa();
+
   } while (opt !== '0');
 
 
--- helpers/inquirer.js
+++ helpers/inquirer.js
@@ -5,20 +5,64 @@ const preguntas = [
     type:'list',
     name:'opcion',
     message: '¿Que opcion deseas hacer?',
-    choices: ['op1', 'op2', 'op3']
+    choices: [
+      {
+        value:'1',
+        name: '1. Crear tarea',
+      },
+      {
+        value:'2',
+        name: '2. Listar tareas',
+      },
+      {
+        value:'3',
+        name: '3. Listar tareas completadas',
+      },
+      {
+        value:'4',
+        name: '4. Listar tareas pendientes',
+      },
+      {
+        value:'5',
+        name: '5. Completar tarea(s)',
+      },
+      {
+        value:'6',
+        name: '6. Borrar tarea',
+      },
+      {
+        value:'0',
+        name: '0. Salir',
+      },
+    ]
   }
 ];
 
+const preg = [
+  {
+    type:'input',
+    name:'pausa',
+    message: `Presione ${'ENTER'.green} para continuar`,
+  }
+];
 const inquirerMenu = async () => {
-  console.clear();
+  // console.clear();
   console.log('==========================');
   console.log(' Seleccione una opcion ');
   console.log('==========================');
 
-  const opt = await inquirer.prompt(preguntas);
-  return opt;
+  const { opcion } = await inquirer.prompt(preguntas);
+  return opcion;
 }
 
+const pausa = async() => {
+
+  return await inquirer.prompt( preg );
+}
+
+
+
+
 module.exports = {
-  inquirerMenu
+  inquirerMenu, pausa
 }
